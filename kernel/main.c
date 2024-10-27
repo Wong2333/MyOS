0e719bdd9a3a1aa3a4c7358a7fb5bbd933ac264e
wjm <3201884732@qq.com>
2024-10-27 12:09:37 +0800

    中断初试

--- kernel/main.c
+++ kernel/main.c
@@ -2,8 +2,8 @@
 #include "init.h"
 void main(void) {
    put_str("I am kernel\n");
-   init_all();
-   asm volatile("sti");	     // 为演示中断处理,在此临时开中断
+   //init_all();
+   //asm volatile("sti");	     // 为演示中断处理,在此临时开中断
    while(1);
 }
 
